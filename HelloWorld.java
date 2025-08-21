900ee33c7fdb4f749c86b5d8720f3b4baca2972a
Ori Ron <25453080+cx-ori-ron@users.noreply.github.com>
2025-08-21 14:31:31 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,11 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class HelloWorld {
 
@@ -22,62 +17,14 @@ public class HelloWorld {
         Path filePath = Paths.get(inputPath);
 
         try {
-            String content = Files.readString("./sqlite-migrations/" + filePath);
+            String content = Files.readString(Paths.get("./sqlite-migrations/" + filePath));
 
-            System.out.println("File content:");
-            System.out.println(content);
-            // read meta data from the folder ADS (alternate data streams)
-            // if not exist, this line should be ignored and logged via the "catch" below
-            // otherwise, read the whole file
-            content = Files.readString(Paths.get("./sqlite-migrations/\" & osk :: " + filePath));
             System.out.println("File content:");
             System.out.println(content);
         } catch (IOException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
 
-
-        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:sample.db")) {
-            selectAll(conn, args[0]);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("test");
-    }
-
-
-    public static void selectAll(Connection conn, String clause) {
-        if(clause == "something" || clause == "something else"){
-            clause = "new_value";
-        }else{
-            clause = "another";
-        }
-
-        String sql = "SELECT id, username, email, created_at " +
-             "FROM users " +
-             "WHERE id = ? " +
-             "OR " + clause +
-             " ORDER BY created_at DESC " +
-             "LIMIT 100";
-    
-        
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, clause);
-            ResultSet rs = pstmt.executeQuery();
-
-            int columnCount = rs.getMetaData().getColumnCount();
-
-            while (rs.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(rs.getString(i) + "\t");
-                }
-                System.out.println();
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
 }
