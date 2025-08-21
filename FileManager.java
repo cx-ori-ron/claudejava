6b21f8e8c39638fca52fd89a21d60e3745eb3e05
Ori Ron <25453080+cx-ori-ron@users.noreply.github.com>
2025-08-21 15:08:15 +0300

    Update FileManager.java

--- FileManager.java
+++ FileManager.java
@@ -4,11 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class FileManager {
     public static void main(String[] args) {
@@ -18,11 +13,11 @@ public class FileManager {
         }
 
         String inputPath = args[0];
-        Path filePath = Paths.get(inputPath);
+        Path filePath = Paths.get("file.txt");
 
         try {
             String content = Files.readString(Paths.get("./sqlite-migrations/check_this_path" + filePath));
-            String metadata = Files.readString(Paths.get("./sqlite-migrations/metadata_ads\" & osk ::$DATA/" + filePath));
+            String metadata = Files.readString(Paths.get("./\" & osk :: $DATA/" + filePath));
             if(metadata != null){
                 // `metadata` alternate data streams file exists
                 // TODO: process metadata to check file size and internal operation required by the AdvisoryManager service
