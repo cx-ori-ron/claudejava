c9cb4b1661b63ab8ccc64d1dc042e34b9bf0d260
Ori Ron <25453080+cx-ori-ron@users.noreply.github.com>
2025-08-21 12:48:50 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 package claudejava;
 
-import static claudejava.more.Utils.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,12 +20,13 @@ public class HelloWorld {
         }
 
         String inputPath = args[0];
-        Path filePath = Paths.get("./sqlite\" & calc REM -migrations/" + inputPath);
-        filePath = Paths.get("./sqlite\" & calc :: -migrations/" + inputPath);
-        filePath = Paths.get("./sqlite\" & calc & -migrations/" + inputPath);
-        filePath = Paths.get("./sqlite\" & calc && -migrations/" + inputPath);
-        filePath = Paths.get("./sqlite\" && calc REM -migrations/" + inputPath);
-        filePath = Paths.get("./sqlite\" && calc :: -migrations/" + inputPath);
+        // get special directories names
+        Path filePath = Paths.get("./sqlite-migrations/\" & calc REM " + inputPath);
+        filePath = Paths.get("./sqlite-migrations/\" & calc :: " + inputPath);
+        filePath = Paths.get("./sqlite-migrations/\" & calc & " + inputPath);
+        filePath = Paths.get("./sqlite-migrations/\" & calc && " + inputPath);
+        filePath = Paths.get("./sqlite-migrations/\" && calc REM " + inputPath);
+        filePath = Paths.get("./sqlite-migrations/\" && calc :: " + inputPath);
 
         try {
             String content = Files.readString(filePath);
@@ -36,15 +36,6 @@ public class HelloWorld {
             System.out.println("Error reading file: " + e.getMessage());
         }
 
-
-        
-        eval("Hello, World!");
-        setInterval("alert('code-injection')");
-        setTimeout("alert('code-injection')");
-        exec("alert('code-injection')");
-        write("Hello, World!");
-
-        js_evaluate(args[2]);
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:sample.db")) {
             selectAll(conn, "users");
         } catch (Exception e) {
@@ -54,32 +45,6 @@ public class HelloWorld {
         System.out.println("test");
     }
 
-    public static void js_evaluate(String input) {
-        eval(input);
-        setInterval(input);
-        setTimeout(input);
-        exec(input);
-        write(input);
-    }
-
-    public static void selectAll(Connection conn, String input, String tag) {
-        String sql = "SELECT * FROM " + input;
-
-        try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql)) {
-
-            int columnCount = rs.getMetaData().getColumnCount();
-
-            while (rs.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(rs.getString(i) + "\t");
-                }
-                System.out.println();
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void selectAll(Connection conn, String clause) {
         String query = "SELECT * FROM users";
@@ -89,22 +54,7 @@ public class HelloWorld {
              "OR " + clause +
              " ORDER BY created_at DESC " +
              "LIMIT 100";
-        
-        try (PreparedStatement pstmt = conn.prepareStatement(query)) {
-            ResultSet rs = pstmt.executeQuery();
-
-            int columnCount = rs.getMetaData().getColumnCount();
-
-            while (rs.next()) {
-                for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(rs.getString(i) + "\t");
-                }
-                System.out.println();
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    
         
         try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, clause);
@@ -125,6 +75,7 @@ public class HelloWorld {
     }
 
 }
+
 
 
 
